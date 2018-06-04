3e9686000688402af1bb193b3d61b4c3a897c0b3
Mateus Ferreira <mateusmferreira@live.com>
2018-06-04 10:30:39 -0300

    Criador de gráficos

--- num.py
+++ num.py
@@ -1,33 +1,69 @@
 import numpy
+import matplotlib.pyplot as plt
 
-a = numpy.array([10, 20, 30, 40])
-mat1 = numpy.array([[1,2],[3,4]])
-mat2 = numpy.array([[5,6],[7,8]])
-mat3 = numpy.array([1,2,3,4])
 
-print(mat1[1][1])
-print(mat1[1,:]) # Exibe a segunda linha da matriz
-print(mat1[:,0]) # Exibe a primeira coluna da matriz
-print(mat1.transpose()) #transforma linhas em colunas
+#a = numpy.array([10, 20, 30, 40])
+#mat1 = numpy.array([[1,2],[3,4]])
+#mat2 = numpy.array([[5,6],[7,8]])
+#mat3 = numpy.array([1,2,3,4])
 
-print(mat1+mat2)
-print(mat1-mat2)
-print(mat1*mat2)
+#print(mat1[1][1])
+#print(mat1[1,:]) # Exibe a segunda linha da matriz
+#print(mat1[:,0]) # Exibe a primeira coluna da matriz
+#print(mat1.transpose()) #transforma linhas em colunas
 
-print(mat3.sum())
-print(mat3.argmax())
-print(mat3.argmin())
+#print(mat1+mat2)
+#print(mat1-mat2)
+#print(mat1*mat2)
 
-l = [20, 30, 10, 40]
+#print(mat3.sum())
+#print(mat3.argmax())
+#print(mat3.argmin())
 
-print(l[1:3]) # Intervaloe entre 1 e 3
+#l = [20, 30, 10, 40]
 
-print(l[::2]) # somente o inicial e o 2
+#print(l[1:3]) # Intervaloe entre 1 e 3
 
-l2 = l[:] # Faz uma cópia de l para l2
-l2[0] = 1000 # Atribui o valor 1000
+#print(l[::2]) # somente o inicial e o 2
 
-print(l)
-print(l2)
+#l2 = l[:] # Faz uma cópia de l para l2
+#l2[0] = 1000 # Atribui o valor 1000
 
-b = a.copy() # Para copiar o array a para o b
+#print(l)
+#print(l2)
+
+#b = a.copy() # Para copiar o array a para o b
+
+#matriz = [[5,4,7],[0,3,4],[0,0,6]]
+
+#mateus = [20,30,40,15]
+#maria = [100,24,48,23]
+#joao = [92,22,34,12]
+#marcos = [12,34,12,43]
+
+#dados = numpy.arange(0,20)
+
+#pontos = numpy.array([mateus,maria,joao,marcos])
+
+#print(pontos)
+#print(pontos[0,:]) # Exibe a segunda linha da matriz
+
+#print(dados)
+
+#new_matriz = numpy.reshape(dados, (5,4))
+#print(new_matriz)
+
+#print(new_matriz.item(8))
+
+#m1 = numpy.array([[1,2,3],[4,5,6]])
+#m2 = numpy.array([[7,8,9],[10,11,12]])
+
+#print(m2/m1)
+
+#print(numpy.matrix.round(m2/m1)) #Faz o aredondamento do cálculo
+
+#print(m1 ** 2) #eleva os valores da matriz por 2
+
+salario = numpy.array([100,200,300,500,400,150]) # criação do array
+plt.plot(salario, c='black',ls='--', marker='s') # Criação do gráfico, seleção dos dados e das caracteristicas
+plt.show() # exibição do gráfico
---
+++ plt.py
@@ -0,0 +1,16 @@
+import numpy
+import matplotlib.pyplot as plt
+
+#salario = numpy.array([100,200,300,500,400,150]) # criação do array
+#plt.plot(salario, c='black',ls='--', marker='s') # Criação do gráfico, seleção dos dados e das caracteristicas
+#plt.show() # exibição do gráfico
+
+#salarios_marcos = numpy.array([100,200,400,300])
+#salarios_gileno = numpy.array([50,300,500,450])
+#salarios_mateus = numpy.array([200,150,300,700])
+
+#plt.plot(salarios_marcos, c='black',ls='--', marker='s', label = 'Salários Marcos')
+#plt.plot(salarios_gileno, c='red',ls='--', marker='^', label = 'Salários Gileno')
+#plt.plot(salarios_mateus, c='green',ls='--', marker='o', label = 'Salários Mateus')
+#plt.legend(loc = 'upper left')
+#plt.show()
