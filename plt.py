8238e1c49f460f4fbb2858c07faf30a9119ffac2
Mateus Ferreira <mateusmferreira@live.com>
2018-06-06 10:42:35 -0300

    teste

--- plt.py
+++ plt.py
@@ -5,12 +5,12 @@ import matplotlib.pyplot as plt
 #plt.plot(salario, c='black',ls='--', marker='s') # Criação do gráfico, seleção dos dados e das caracteristicas
 #plt.show() # exibição do gráfico
 
-#salarios_marcos = numpy.array([100,200,400,300])
-#salarios_gileno = numpy.array([50,300,500,450])
-#salarios_mateus = numpy.array([200,150,300,700])
+salarios_marcos = numpy.array([100,200,400,300])
+salarios_gileno = numpy.array([50,300,500,450])
+salarios_mateus = numpy.array([200,150,300,700])
 
-#plt.plot(salarios_marcos, c='black',ls='--', marker='s', label = 'Salários Marcos')
-#plt.plot(salarios_gileno, c='red',ls='--', marker='^', label = 'Salários Gileno')
-#plt.plot(salarios_mateus, c='green',ls='--', marker='o', label = 'Salários Mateus')
-#plt.legend(loc = 'upper left')
-#plt.show()
+plt.plot(salarios_marcos, c='black',ls='--', marker='s', label = 'Salários Marcos')
+plt.plot(salarios_gileno, c='red',ls='--', marker='^', label = 'Salários Gileno')
+plt.plot(salarios_mateus, c='green',ls='--', marker='o', label = 'Salários Mateus')
+plt.legend(loc = 'upper left')
+plt.show()
